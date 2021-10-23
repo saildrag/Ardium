367f95bdb6469d16be9437f582167ecf8a6845ca
saildrag <89639054+saildrag@users.noreply.github.com>
2021-10-23 12:34:50 +0200

    push

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,5 +1,5 @@
-const APP_DATA_NAME = 'ModRealmsLauncher'
-const LIVE_DISTRIBUTION_URL = 'https://launcher.modrealms.net/deploy/distribution.json'
+const APP_DATA_NAME = 'Ardium'
+const LIVE_DISTRIBUTION_URL = 'https://raw.githubusercontent.com/saildrag/Launcher/master/docs/distribution.json'
 const DEV_DISTRIBUTION_URL = 'https://launcher.modrealms.net/dev-deploy/distribution.json'
 
 module.exports = { APP_DATA_NAME, LIVE_DISTRIBUTION_URL, DEV_DISTRIBUTION_URL }
